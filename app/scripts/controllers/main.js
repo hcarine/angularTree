0ebd3bdca38decb6e97c8140288532277cb798fc
Carine <hammescarine@gmail.com>
2017-07-27 08:44:18 -0300

    start project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTreeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTreeApp
+ */
+angular.module('angularTreeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
